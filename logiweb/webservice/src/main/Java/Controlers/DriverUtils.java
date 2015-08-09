53b7d250d07000f53a8fc4255f017095aaa61d20
unknown <dehasi@yandex.ru>
2015-08-09 23:41:50 +0300

    DAO problems

---
+++ logiweb/webservice/src/main/Java/Controlers/DriverUtils.java
@@ -0,0 +1,112 @@
+package Controlers;
+
+import businessLogic.*;
+import com.google.gson.Gson;
+import model.Driver;
+import model.statuses.BaggageStatus;
+import model.statuses.DriverStatus;
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * utils for rest requests
+ */
+
+public class DriverUtils {
+
+    Logger logger = Logger.getLogger(DriverUtils.class);
+
+    public ResponseEntity<String> changeDriverStatus( String id,String status ){
+
+
+        try {
+
+            int driverId = Integer.parseInt(id);
+
+            DriverStatus driverStatus = DriverStatus.fromString(status);
+
+            DriverService driverService = BusinessFactory.getInstance().getDriverLogic();
+
+            driverService.setDtiverStatus(driverId, driverStatus);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (Exception e) {
+            logger.error(e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    public ResponseEntity<String> changeOrderStatus(String id,String status) {
+
+        try {
+            int baggageId = Integer.parseInt(id);
+
+            BaggageStatus baggageStatus = BaggageStatus.fromString(status);
+
+            OrderService service = BusinessFactory.getInstance().getOrderLogic();
+
+            service.changeBaggageStatus(baggageId, baggageStatus);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (Exception e) {
+            logger.error(e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    public String getDriverViewJSON(String iD) {
+        DriverView view = getDriverView(iD);
+        Gson gson = new Gson();
+        Driver d = view.getDriver();
+        String res;
+        try {
+            res = gson.toJson(view);
+        } catch (Exception ignore) {
+            res = view.toString();
+        }
+        return res;
+    }
+    public DriverView getDriverView(String iD) {
+        int id = Integer.parseInt(iD);
+        DriverService driverService = BusinessFactory.getInstance().getDriverLogic();
+        OrderService orderService = BusinessFactory.getInstance().getOrderLogic();
+
+        Driver driver = driverService.getById(id);
+
+        if(driver == null)
+            return null;
+
+        DriverView driverView = new DriverView();
+        driverView.setDriver(driver);
+
+        Integer orderId = driver.getOrderRoute();
+
+        if(orderId != null) {
+            List<Driver> friends = new ArrayList<>();
+            friends.addAll(driverService.getFriends(id));
+            friends.remove(driver);
+
+            List<OrderView> route = orderService.getOrderView(orderId);
+
+            driverView.setFriends(friends);
+            driverView.setRoute(route);
+
+            String truckId = orderService.getTruckIdByOrder(orderId);
+            driverView.setOrderId(orderId);
+            driverView.setTruckId(truckId);
+        }
+
+
+
+        return driverView;
+    }
+}
---
+++ logiweb/webservice/src/main/Java/Controlers/RemoteServiceController.java
@@ -0,0 +1,53 @@
+package Controlers;
+
+import businessLogic.DriverView;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Driver;
+
+/**
+ * controoler for ralking with clients
+ */
+@RestController
+@RequestMapping("/rest")
+//@Produces(MediaType.APPLICATION_JSON)
+//@Consumes(MediaType.APPLICATION_JSON)
+public class RemoteServiceController {
+    private static Logger logger = Logger.getLogger(RemoteServiceController.class);
+
+    private DriverUtils driverUtils = new DriverUtils();
+
+    @RequestMapping(value = "/driver/status")
+    ResponseEntity<String> updateDriverStatus(@RequestParam String id, @RequestParam String status) {
+        logger.info("requested /driver/status");
+        return driverUtils.changeDriverStatus(id, status);
+    }
+
+    @RequestMapping(value = "/baggage/status")
+    ResponseEntity<String> updateBaggageStatus(@RequestParam String id,@RequestParam String status) {
+        logger.info("requested /baggage/status");
+        return driverUtils.changeOrderStatus(id, status);
+    }
+
+    @RequestMapping(value = "/driver/view")
+    @ResponseBody
+    String getDriverView(@RequestParam String login, @RequestParam String password) {
+        logger.info("requested /driver/view");
+//        DriverView view =driverUtils.getDriverView(login);
+
+//        logger.info(view);
+        String res = driverUtils.getDriverViewJSON(login);
+        return  res;
+    }
+
+
+}
